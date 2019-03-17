6a524355d87d7e43936fd84b0f9d971a134fee36
Abdulrahman Alassadi <alassadi@live.com>
2019-03-17 16:33:10 +0100

    enpoint to return moving or idle data for a given ID and date

--- app/data/index.js
+++ app/data/index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser')();
 const express = require('express');
 const app = express();
-const auth = require('../auth');
+//const auth = require('../auth');
 const firebaseHelper = require('firebase-functions-helper');
 const db = admin.firestore();
 app.use(require('cors')({
@@ -12,11 +12,11 @@ app.use(require('cors')({
 }));
 app.use(cookieParser);
 app.use(bodyParser.json());
-app.use(auth);
+//app.use(auth);
 
 /**
  * Gets ALL the documents in the data collection
- * To get all documents in the house, send a GET request such as:
+ * To get all documents in the database, send a GET request such as:
  * http://localhost:5000/cloudproject-f25f2/us-central1/data
  */
 
@@ -43,6 +43,38 @@ app.get('/:id', (req, res) => {
       return res.status(500).json({
         message: 'Error' + error.toString()
       });
+    });
+});
+
+/** 
+ * With URL parameters:
+ * Send a GET request such as
+ * http://localhost:5000/cloudproject-f25f2/us-central1/data/nB8wIVYFXe8XRVRagwG6/2019-04-01/moving
+ * to read the 'moving' data from the passed ID and Date
+ * or 
+ * http://localhost:5000/cloudproject-f25f2/us-central1/data/nB8wIVYFXe8XRVRagwG6/2019-04-01/idle
+ * to read the 'idle' data from the passed ID and Date
+ * @type {HttpsFunction}
+ */
+
+app.get('/:id/:date/:state', (req, res) => {
+  const id = req.params.id;
+  const date = req.params.date;
+  const state = req.params.state;
+  const messageRef = db.collection('data').doc(id)
+    .collection(date).doc(state);
+  messageRef.get()
+    .then(doc => {
+      if (!doc.exists) {
+        return res.status(404).json({
+          message: 'Document Not Found!'
+        });
+      } else {
+        return res.status(200).send(doc.data());
+      }
+    })
+    .catch(err => {
+      console.log('Error getting document', err);
     });
 });
 
